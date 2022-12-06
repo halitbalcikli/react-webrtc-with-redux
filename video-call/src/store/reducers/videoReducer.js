1d6ce7f5633e32f90c144db0848bdb2ce309762d
Mehmet Halid Balçıklı <halit_balcikli@hotmail.com>
2022-12-06 21:58:31 +0300

    change some log

--- video-call/src/store/reducers/videoReducer.js
+++ video-call/src/store/reducers/videoReducer.js
@@ -24,22 +24,3 @@ export const username = state => state.video.username;
 export const activeUsers = state => state.video.activeUsers;
 
 export default videoSlice.reducer;
-
-
-// const reducer = (state = initState, action) => {
-//   switch (action.type) {
-//     case dashboardActions.DASHBOARD_SET_USERNAME:
-//       return {
-//         ...state,
-//         username: action.username
-//       };
-//     case dashboardActions.DASHBOARD_SET_ACTIVE_USERS:
-//       return {
-//         ...state,
-//         activeUsers: action.activeUsers
-//       };
-//     default:
-//       return state;
-//   }
-// }
-// ;
